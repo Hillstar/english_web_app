12ffaf8719005bb586ac94fe7326e91588e7e9fc
Hillstar <61278508+Hillstar@users.noreply.github.com>
2020-08-15 20:54:58 +0700

    Closed #4; Created Vue project and added simple welcome page

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Greeting msg="Soon you will be able to learn English here!"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Greeting from '@/components/Greeting.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Greeting
+  }
+}
+</script>
