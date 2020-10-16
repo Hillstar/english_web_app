e1a85d2db90fc58f331dcc0b800634bcd16d79ef
Hillstar <61278508+Hillstar@users.noreply.github.com>
2020-10-16 21:26:15 +0700

    Resolves #13; Added a simple backend

---
+++ client/src/components/Choise.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <button @click="$emit('submit-ans', choice.id)"
+        class="submit-answer"
+        v-bind:class="{'correctStyle':(showAnswer && rightAnswer==choice.id), 
+                    'incorrectStyle':(showAnswer && rightAnswer!=choice.id)}"
+        v-bind:disabled="showAnswer">
+        {{ choice.text }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Choise",
+    props: ["choice", 
+            "rightAnswer",
+            "showAnswer"]
+}
+</script>
+
+<style scoped>
+    .correctStyle {
+        background-color: #2ecc71;
+    }
+
+    .incorrectStyle {
+        background-color: #e74c3c;
+    }
+
+    .incorrectStyle, .correctStyle{
+        cursor: default;
+    }
+</style>
